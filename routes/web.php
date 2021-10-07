f225ab22be4a9f2399c0679a039be3a4257ca16c
Elijah Korir <elijahkiptoo98@gmail.com>
2021-10-07 10:09:42 +0300

    Make router more readable

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IssuesController;
+use App\Http\Controllers\NotesController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\RemindersController;
+use App\Http\Controllers\RepositoryController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,61 +24,62 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Auth::routes();
-Route::get('/login', [App\Http\Controllers\AuthController::class, 'login_view']);
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
-Route::get('/register', [App\Http\Controllers\AuthController::class, 'register_view']);
-Route::post('/register', [App\Http\Controllers\AuthController::class, 'register'])->name('register');
-Route::get('/test', [App\Http\Controllers\TestingController::class, 'test']);
-Route::get('/test_2', [App\Http\Controllers\TestingController::class, 'test_2']);
-Route::get('/test_3', [App\Http\Controllers\TestingController::class, 'fake_data']);
-Route::get('/test_notify', [App\Http\Controllers\TestingController::class, 'notify']);
-Route::get('/github_callback', [App\Http\Controllers\ProfileController::class, 'callback']);
+Route::get('/login', [AuthController::class, 'login_view']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register_view']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/test', [TestingController::class, 'test']);
+Route::get('/test_2', [TestingController::class, 'test_2']);
+Route::get('/test_3', [TestingController::class, 'fake_data']);
+Route::get('/test_notify', [TestingController::class, 'notify']);
+Route::get('/github_callback', [ProfileController::class, 'callback']);
 
 // Only Authorised and Verified users
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'profile'])->name('profile');
-    Route::post('/profile_pic', [App\Http\Controllers\ProfileController::class, 'profile_photo'])->name('profile_pic');
-    Route::post('/change_pass', [App\Http\Controllers\ProfileController::class, 'change_password'])->name('change_pass');
-    Route::post('/add_access', [App\Http\Controllers\ProfileController::class, 'add_token'])->name('add_pan');
-    Route::get('/recent_activities', [App\Http\Controllers\ProfileController::class, 'recent_activity'])->name('recent_activity');
-    Route::get('/token_list', [App\Http\Controllers\ProfileController::class, 'tokens_list'])->name('token_list');
-    Route::post('/remove_token', [App\Http\Controllers\ProfileController::class, 'remove_token'])->name('delete_token');
-    Route::get('/task', [App\Http\Controllers\TaskController::class, 'task_view'])->name('task_view');
-    Route::get('/get_tasks', [App\Http\Controllers\TaskController::class, 'get_tasks'])->name('get_tasks');
-    Route::post('/add_task', [App\Http\Controllers\TaskController::class, 'create_task'])->name('add_task');
-    Route::post('/edit_task', [App\Http\Controllers\TaskController::class, 'edit_task'])->name('edit_task');
-    Route::post('/remove_task', [App\Http\Controllers\TaskController::class, 'delete_task'])->name('delete_task');
-    Route::get('/repositories', [App\Http\Controllers\RepositoryController::class, 'repositories_view'])->name('repositories');
-    Route::get('/notes', [App\Http\Controllers\NotesController::class, 'notes_view'])->name('notes_view');
-    Route::post('/add_note', [App\Http\Controllers\NotesController::class, 'create_note'])->name('add_note');
-    Route::post('/edit_note', [App\Http\Controllers\NotesController::class, 'edit_note'])->name('edit_note');
-    Route::get('/get_note', [App\Http\Controllers\NotesController::class, 'get_specific_note'])->name('get_note');
-    Route::get('/get_all_notes', [App\Http\Controllers\NotesController::class, 'get_notes'])->name('get_all_notes');
-    Route::post('/delete_note', [App\Http\Controllers\NotesController::class, 'delete_note'])->name('delete_note');
-    Route::get('/reminders', [App\Http\Controllers\RemindersController::class, 'reminders_view'])->name('reminders_view');
-    Route::post('/add_reminder', [App\Http\Controllers\RemindersController::class, 'create_reminder'])->name('add_reminder');
-    Route::get('/get_all_reminders', [App\Http\Controllers\RemindersController::class, 'get_reminders'])->name('get_all_reminders');
-    Route::get('/get_reminder', [App\Http\Controllers\RemindersController::class, 'get_specific_reminder'])->name('get_reminder');
-    Route::post('/edit_reminder', [App\Http\Controllers\RemindersController::class, 'edit_reminder'])->name('edit_reminder');
-    Route::post('/delete_reminder', [App\Http\Controllers\RemindersController::class, 'delete_reminder'])->name('delete_reminder');
-    Route::get('/repository/{id}', [App\Http\Controllers\RepositoryController::class, 'specific_repository'])->name('view_specific_repository');
-    Route::get('/get_projects', [App\Http\Controllers\ProjectsController::class, 'get_projects'])->name('get_projects');
-    Route::post('/add_project', [App\Http\Controllers\ProjectsController::class, 'create_project'])->name('add_project');
-    Route::get('/get_recent_notifications', [App\Http\Controllers\HomeController::class, 'get_top_three_notifications'])->name('fetch_notifications');
-    Route::get('/repository/{id}/issues', [App\Http\Controllers\RepositoryController::class, 'fetch_issues_in_repository'])->name('fetch_issues_in_repo');
-    Route::post('/add_new_issue', [App\Http\Controllers\IssuesController::class, 'createIssue'])->name('add_new_issue');
-    Route::get('/project/{id}', [App\Http\Controllers\ProjectsController::class, 'specificProject'])->name('view_specific_project');
-    Route::get('/mark_as_read/{id}', [App\Http\Controllers\HomeController::class, 'markNotificationAsRead'])->name('mark_as_read');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
+    Route::post('/profile_pic', [ProfileController::class, 'profile_photo'])->name('profile_pic');
+    Route::post('/change_pass', [ProfileController::class, 'change_password'])->name('change_pass');
+    Route::post('/add_access', [ProfileController::class, 'add_token'])->name('add_pan');
+    Route::get('/recent_activities', [ProfileController::class, 'recent_activity'])->name('recent_activity');
+    Route::get('/token_list', [ProfileController::class, 'tokens_list'])->name('token_list');
+    Route::post('/remove_token', [ProfileController::class, 'remove_token'])->name('delete_token');
+    Route::get('/task', [TaskController::class, 'task_view'])->name('task_view');
+    Route::get('/get_tasks', [TaskController::class, 'get_tasks'])->name('get_tasks');
+    Route::post('/add_task', [TaskController::class, 'create_task'])->name('add_task');
+    Route::post('/edit_task', [TaskController::class, 'edit_task'])->name('edit_task');
+    Route::post('/remove_task', [TaskController::class, 'delete_task'])->name('delete_task');
+    Route::get('/repositories', [RepositoryController::class, 'repositories_view'])->name('repositories');
+    Route::get('/notes', [NotesController::class, 'notes_view'])->name('notes_view');
+    Route::post('/add_note', [NotesController::class, 'create_note'])->name('add_note');
+    Route::post('/edit_note', [NotesController::class, 'edit_note'])->name('edit_note');
+    Route::get('/get_note', [NotesController::class, 'get_specific_note'])->name('get_note');
+    Route::get('/get_all_notes', [NotesController::class, 'get_notes'])->name('get_all_notes');
+    Route::post('/delete_note', [NotesController::class, 'delete_note'])->name('delete_note');
+    Route::get('/reminders', [RemindersController::class, 'reminders_view'])->name('reminders_view');
+    Route::post('/add_reminder', [RemindersController::class, 'create_reminder'])->name('add_reminder');
+    Route::get('/get_all_reminders', [RemindersController::class, 'get_reminders'])->name('get_all_reminders');
+    Route::get('/get_reminder', [RemindersController::class, 'get_specific_reminder'])->name('get_reminder');
+    Route::post('/edit_reminder', [RemindersController::class, 'edit_reminder'])->name('edit_reminder');
+    Route::post('/delete_reminder', [RemindersController::class, 'delete_reminder'])->name('delete_reminder');
+    Route::get('/repository/{id}', [RepositoryController::class, 'specific_repository'])->name('view_specific_repository');
+    Route::get('/get_projects', [ProjectsController::class, 'get_projects'])->name('get_projects');
+    Route::post('/add_project', [ProjectsController::class, 'create_project'])->name('add_project');
+    Route::get('/get_recent_notifications', [HomeController::class, 'get_top_three_notifications'])->name('fetch_notifications');
+    Route::get('/repository/{id}/issues', [RepositoryController::class, 'fetch_issues_in_repository'])->name('fetch_issues_in_repo');
+    Route::post('/add_new_issue', [IssuesController::class, 'createIssue'])->name('add_new_issue');
+    Route::get('/project/{id}', [ProjectsController::class, 'specificProject'])->name('view_specific_project');
+    Route::get('/mark_as_read/{id}', [HomeController::class, 'markNotificationAsRead'])->name('mark_as_read');
+    Route::get('/project/{id}/notes', [NotesController::class, 'getNotesSpecificProject'])->name('get_notes_specific_project');
 });
 
 // Email Verification Routes
 Route::middleware(['auth'])->group(function () {
-    Route::get('/email/verify', [App\Http\Controllers\AuthController::class, 'verification_view'])->name('verification.notice');
+    Route::get('/email/verify', [AuthController::class, 'verification_view'])->name('verification.notice');
 
-    Route::get('/email/verify/{id}/{hash}', [App\Http\Controllers\AuthController::class, 'handle_verification_request'])->name('verification.verify');
+    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'handle_verification_request'])->name('verification.verify');
 });
 
-Route::post('/email/verification-notification', [App\Http\Controllers\AuthController::class, 'send_verification'])->middleware(['throttle:6,1', 'auth'])->name('verification.send');
-Route::post('/email/resend-verification', [App\Http\Controllers\AuthController::class, 'send_verification'])->middleware(['throttle:6,1', 'auth'])->name('verification.resend');
+Route::post('/email/verification-notification', [AuthController::class, 'send_verification'])->middleware(['throttle:6,1', 'auth'])->name('verification.send');
+Route::post('/email/resend-verification', [AuthController::class, 'send_verification'])->middleware(['throttle:6,1', 'auth'])->name('verification.resend');
