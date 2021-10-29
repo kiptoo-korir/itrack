e0dc37d099c3f646fb32733634522233ed1dee15
Elijah Korir <elijahkiptoo98@gmail.com>
2021-10-29 10:23:59 +0300

    Invalid token notification complete

---
+++ app/Notifications/InvalidTokenNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Services\NotificationService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class InvalidTokenNotification extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+    protected NotificationService $notificationService;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(NotificationService $notificationService)
+    {
+        $this->notificationService = $notificationService;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     */
+    public function via($notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            'notification_message' => 'You currently don\'t have a valid token to facilitate access to Github, you can generate a new one here:',
+            'notification_title' => 'Invalid Github Token',
+            'notification_type' => 'invalidToken',
+            'action_link' => route('profile'),
+        ];
+    }
+
+    public function toBroadcast($notifiable): BroadcastMessage
+    {
+        $count = $this->notificationService->getNotificationCount($notifiable->id);
+
+        return new BroadcastMessage([
+            'notifications_count' => $count,
+            'id' => $this->id,
+            'notification_message' => 'You currently don\'t have a valid token to facilitate access to Github, you can generate a new one here:',
+            'notification_title' => 'Invalid Github Token',
+            'notification_type' => 'invalidToken',
+            'action_link' => route('profile'),
+        ]);
+    }
+}
