c57daa217266b4621cacff357c57df5ba3b452b3
Elijah Korir <elijahkiptoo98@gmail.com>
2021-11-02 22:43:42 +0300

    Create service methods to check for token issues

--- app/Services/UserDataService.php
+++ app/Services/UserDataService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\AccessToken;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -17,5 +18,32 @@ class UserDataService
             ->whereNull('read_at')
             ->first()->count
         ;
+    }
+
+    public function checkGithubTokenStatus(int $userId): bool
+    {
+        $validTokenCount = AccessToken::where(['platform' => 1, 'owner' => $userId, 'verified' => true])
+            ->count()
+        ;
+
+        if (0 === $validTokenCount) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function checkForPreviousTokens(int $userId): bool
+    {
+        $validTokenCount = AccessToken::where(['platform' => 1, 'owner' => $userId, 'verified' => true])
+            ->withTrashed()
+            ->count()
+        ;
+
+        if (0 === $validTokenCount) {
+            return false;
+        }
+
+        return true;
     }
 }
