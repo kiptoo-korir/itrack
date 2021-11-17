de33138843d9d2c7d7e75cc76fb9bf6a87df78ba
Elijah Korir <elijahkiptoo98@gmail.com>
2021-11-17 21:36:49 +0300

    Edit repositories view - to make it more friendly

--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -11,28 +11,42 @@
             box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.5) !important;
         }
 
+        .card-footer-custom {
+            padding: 0.75rem 1.25rem;
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+
+        .card-footer-custom:last-child {
+            border-radius: 0 0 calc(0.25rem - 1px) calc(0.25rem - 1px);
+        }
+
+        .card-text {
+            font-size: 0.95rem;
+        }
+
     </style>
 @endsection
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container">
+        <h4 class="text-black-50 mr-auto pb-3">Repositories</h4>
         @if (count($repositories) === 0)
             <div class="alert alert-info" role="alert" id="alert_1">
                 There is currently no information related to your repositories stored within the platform.
             </div>
         @endif
-        <div class="row row-cols-1 row-cols-md-3" id="repo_container">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3" id="repo_container">
             @forelse ($repositories as $repo)
                 <div class="col mb-3">
                     <div class="card bg-card shadow h-100">
-                        <div class="card-header">{{ $repo->name }}</div>
                         <div class="card-body">
-                            <p class="card-text">Description: {{ $repo->description }}</p>
-                            <p class="card-text">Created On: {{ $repo->date_created_online }}</p>
-                            <p class="card-text">Updated On: {{ $repo->date_updated_online }}</p>
+                            <h5><b>{{ $repo->name }}</b></h5>
+                            <p>{{ $repo->description }}</p>
+                            <p class="card-text">Created {{ $repo->date_created_online }}</p>
+                            <p class="card-text">Updated {{ $repo->date_updated_online }}</p>
                             <i class="bi bi-journal-x"></i><span> {{ $repo->issues_count }} Issues</span>
                         </div>
-                        <div class="card-footer">
+                        <div class="card-footer-custom">
                             <a class="btn btn-primary btn-sm"
                                 href="{{ route('view_specific_repository', $repo->id) }}">Open</a>
                         </div>
@@ -73,14 +87,14 @@
 
             let elementContent = `
                 <div class="card bg-card shadow h-100">
-                    <div class="card-header">${item.name}</div>
                     <div class="card-body">
-                        <p class="card-text">Description: ${item.description}</p>
-                        <p class="card-text">Created On: ${createdDate.toUTCString()}</p>
-                        <p class="card-text">Updated On: ${updatedDate.toUTCString()}</p>
+                        <h5><b>${item.name}</b></h5>
+                        <p>${item.description}</p>
+                        <p class="card-text">Created ${createdDate.toDateString()}</p>
+                        <p class="card-text">Updated ${updatedDate.toDateString()}</p>
                         <i class="bi bi-journal-x"></i><span> ${item.issues_count} Issues</span>
                     </div>
-                    <div class="card-footer">
+                    <div class="card-footer-custom">
                         <a class="btn btn-primary btn-sm" href="${repoRoute}">Open</a>
                     </div>
                 </div>`;
