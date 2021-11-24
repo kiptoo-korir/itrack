1d25a758d01ed00a3d9c0de775a924ca27d94303
Elijah Korir <elijahkiptoo98@gmail.com>
2021-11-24 11:30:52 +0300

    Added all notifications view to view composer

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -20,7 +20,7 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $gitTokenComposerViews = ['home', 'profile', 'reminder', 'project', 'note', 'task', 'repositories', 'specific_repository'];
+        $gitTokenComposerViews = ['home', 'profile', 'reminder', 'project', 'note', 'task', 'repositories', 'specific_repository', 'all-notifications'];
         view()->composer($gitTokenComposerViews, GitTokenComposer::class);
         view()->composer($gitTokenComposerViews, UserDataComposer::class);
     }
