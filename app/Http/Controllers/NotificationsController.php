89d59191f36eaeb4e152c31b377dc86c6a16c34f
Elijah Korir <elijahkiptoo98@gmail.com>
2021-12-01 11:43:31 +0300

    All notifications functionality completed

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -18,26 +18,31 @@ class NotificationsController extends Controller
         $userId = Auth::id();
         $limit = 50;
         $offset = ($page - 1) * $limit;
-        $notifications = DB::table('notifications')->select('data', 'created_at', 'id')
+        $notifications = DB::table('notifications')->select('data', 'created_at', 'id', 'read_at')
             ->where([
                 'notifiable_type' => 'App\Models\User',
                 'notifiable_id' => $userId,
             ])
-            ->whereNull('read_at')
             ->offset($offset)
             ->limit($limit)
             ->orderBy('created_at', 'desc')
             ->get()
         ;
 
+        $notificationCount = $notifications->count();
+        $endOfPagination = ($limit * $page > $notificationCount) ? $notificationCount : ($limit * $page);
+
+        $paginationString = ($offset * $limit + 1).'-'.$endOfPagination.' of '.$notificationCount;
+
         $notificationsDecoded = $notifications->map(function ($notification) {
             $notificationData = json_decode($notification->data);
             $notificationData->created_at = $notification->created_at;
             $notificationData->id = $notification->id;
+            $notificationData->read_at = $notification->read_at;
 
             return $notificationData;
         });
 
-        return response()->json(['notifications' => $notificationsDecoded], 200);
+        return response()->json(['notifications' => $notificationsDecoded, 'paginationString' => $paginationString], 200);
     }
 }
--- resources/views/all-notifications.blade.php
+++ resources/views/all-notifications.blade.php
@@ -7,11 +7,79 @@
             color: #000000;
         }
 
+        .text-95 {
+            font-size: 95%;
+        }
+
+        .text-bold {
+            font-weight: bold;
+        }
+
+        .text-bold h6 {
+            font-weight: bold;
+        }
+
+        .unread {
+            border-left: 5px solid #b80728;
+        }
+
+        .text-align-end {
+            text-align: end;
+        }
+
+        #filter-dropdown {
+            z-index: 10;
+        }
+
+        .hide {
+            display: none;
+        }
+
     </style>
 @endsection
 
 @section('content')
     <div class="container">
+        <h3 class="text-black">All Notifications</h3>
+        <div class="position-sticky border-bottom pb-2 mb-3">
+            <div class="row justify-content-between">
+                <div class="col align-self-center">
+                    <div class="row">
+                        <div class="col-6">
+                            <select name="" id="select-filter" class="form-control">
+                                <option value="" disabled selected>Filters</option>
+                                <option value="all">Show All</option>
+                                <option value="unread">Show Unread</option>
+                                <option value="read">Show Read</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col align-self-center">
+                    <div class="text-align-end">
+                        <span class="text-95 mr-3" id="pagination-string"></span>
+                        <button type="button" class="btn btn-sm btn-outline-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-chevron-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z">
+                                </path>
+                            </svg>
+                            {{-- <span class="visually-hidden">Newer</span> --}}
+                        </button>
+                        <button type="button" class="btn btn-sm btn-outline-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+                                </path>
+                            </svg>
+                            {{-- <span class="visually-hidden">Older</span> --}}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="bg-white rounded" id="notifications-container">
 
         </div>
@@ -25,36 +93,47 @@
 @section('js_scripts')
     <script>
         async function fetchUnreadNotifications(page) {
+            showSpinner();
             const route = `{{ route('get-notifications', '') }}/${page}`;
             const response = await fetch(route);
             const body = await response.json();
             const {
-                notifications
+                notifications,
+                paginationString
             } = body;
+            hideSpinner();
             notifications.forEach(loadNotifications);
+            document.getElementById('pagination-string').innerHTML = paginationString;
+            filterNotifications();
         }
 
         function loadNotifications(notification, index) {
-
-            
             const {
+                id,
                 notification_message: message,
                 notification_title: title,
-                created_at
+                created_at,
+                read_at
             } = notification;
 
+            const elementClass = (read_at === null) ? 'text-bold unread' : 'read';
             let element = '';
-            element +=
-                `<div class="list-group list-group-flush bg-gradient-lighter border-bottom text-black-50 text-bold">`;
-            element += `<a href="#!" class="list-group-item list-group-item-action">`;
-            element += `<div class="row align-items-center">`;
-            element += `<div class="col ml--2">`;
-            element += `<div class="d-flex justify-content-between align-items-center">`;
-            element += `<div><h6 class="mb-0 text-sm">${title}</h6></div>`;
-            element +=
-                `<div class="text-right text-muted"><small>${time_difference(created_at)}</small></div>`;
-            element += `</div><p class="mb-0">${message}</p></div>`;
-            element += `</div></a></div>`;
+            element =
+                `<div class="list-group list-group-flush border-bottom ${elementClass} individual-not" id="not-view-${id}">
+                    <div class="list-group-item list-group-item-action">
+                        <div class="row align-items-center">
+                            <div class="col ml--2">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div><h6 class="mb-0 text-sm text-black">${title}</h6></div>
+                                    <div class="text-right text-muted"><small>${time_difference(created_at)}</small></div>
+                                </div>
+                                <p class="mb-0 text-black text-95">${message}</p>
+                            </div>
+                        </div>
+                        <div class="text-right text-notification"><a class="mark-as-read" href="javascript:void(0)"
+                                onclick="changeReadStatus('${id}')">Mark as read</a></div>
+                    </div>
+                </div>`;
             $('#notifications-container').append(element);
         }
 
@@ -82,6 +161,56 @@
             }
 
             return time_diff;
+        }
+
+        function changeReadStatus(notificationId) {
+            const notElement = document.getElementById(`not-view-${notificationId}`);
+            notElement.classList.remove('text-bold', 'unread');
+            notElement.classList.add('read');
+
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('select-filter').value = 'all';
+            fetchUnreadNotifications(1);
+        });
+
+        document.getElementById('select-filter').addEventListener('change', filterNotifications);
+
+        function showAllNotifications() {
+            let elements = document.querySelectorAll('.individual-not');
+            elements.forEach((element) => {
+                element.classList.remove('hide');
+            });
+        }
+
+        function filterNotifications() {
+            const filterState = document.getElementById('select-filter').value;
+
+            if (filterState === 'all') {
+                showAllNotifications();
+                return;
+            }
+
+            if (filterState === 'read') {
+                showAllNotifications();
+                let elements = document.querySelectorAll('.unread');
+                elements.forEach((element) => {
+                    element.classList.remove('hide');
+                    element.classList.add('hide');
+                });
+                return;
+            }
+
+            if (filterState === 'unread') {
+                showAllNotifications();
+                let elements = document.querySelectorAll('.read');
+                elements.forEach((element) => {
+                    element.classList.remove('hide');
+                    element.classList.add('hide');
+                });
+                return;
+            }
         }
     </script>
 @endsection
